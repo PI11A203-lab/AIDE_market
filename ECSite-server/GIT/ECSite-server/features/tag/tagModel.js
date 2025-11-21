31b29ebb1f9693ad679d6ad7522997f9a4a69340
Secondyz <sdef2da1@gmail.com>
2025-11-21 21:04:37 +0900

    project内でもエンドポイントが複数できてしまう機能の場合 参照元の機能ごとにファイルわけしておくと分かりやすくていいよねっていう変更。

--- ECSite-server/GIT/ECSite-server/features/tag/tagModel.js
+++ ECSite-server/GIT/ECSite-server/features/tag/tagModel.js
@@ -1,6 +1,6 @@
 // features/tag/tagModel.js
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("Tag", {
+    const Tag = sequelize.define("Tag", {
         name: {
             type: DataTypes.STRING(50),
             allowNull: false,
@@ -11,5 +11,18 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: true,
         },
     });
+
+    // アソシエーション（関連付け）を定義
+    Tag.associate = (models) => {
+        // Tag - Product: 多対多
+        Tag.belongsToMany(models.Product, {
+            through: 'ProductTags',
+            foreignKey: 'tag_id',
+            otherKey: 'product_id',
+            as: 'products'
+        });
+    };
+
+    return Tag;
 };
 
